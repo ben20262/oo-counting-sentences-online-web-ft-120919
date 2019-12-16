b475d5e6c8555f0d83befaac9b0e02338e2c4a05
Benjamin Small <ben20262@aim.com>
2019-12-16 20:03:51 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count = 0
+    self.split.each_index do |i|
+      puts i
+      word = self.split[i]
+      puts word
+      if word.include? "!"
+        count += 1
+      elsif word.include? "?"
+        count += 1
+      elsif word.include? "."
+        count += 1
+      end
+    end
+    count
   end
 end
