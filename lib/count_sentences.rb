48879a997bccaf8e5af9a8f325d205aea4636315
Benjamin Small <ben20262@aim.com>
2019-12-16 19:46:46 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -21,10 +21,8 @@ class String
       p_letter = self[i - 1]
       puts "Letter is #{letter}"
       puts "P is #{p_letter}"
-      if letter == "!" || letter == "." || letter == "?"
-        if p_letter != "!" && p_letter != "." && p_letter != "?"
+      if letter == "!" || letter == "." || letter == "?" && p_letter != "!" || p_letter != "." || p_letter != "?"
           count += 1
-        end
       end
     end
     count
