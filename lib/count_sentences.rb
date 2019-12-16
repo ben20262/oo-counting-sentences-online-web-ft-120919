ff89450aebb6de35d2436da9bc410c63ab8b51c1
Benjamin Small <ben20262@aim.com>
2019-12-16 19:50:26 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,10 +17,8 @@ class String
   def count_sentences
     count = 0
     self.split.each_index do |i|
-      letter = self[i]
-      p_letter = self[i - 1]
-      puts "Letter is #{letter}"
-      puts "P is #{p_letter}"
+      letter = self.split[i]
+      p_letter = self.split[i - 1]
       if letter == "!" || letter == "." || letter == "?" && p_letter != "!" || p_letter != "." || p_letter != "?"
           count += 1
       end
