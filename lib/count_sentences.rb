f0e955375c77ebeaaaf06ff35a122d694f04c998
Benjamin Small <ben20262@aim.com>
2019-12-16 19:41:11 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,14 @@ class String
   end
 
   def count_sentences
-
+    count = 0
+    self.split.each_index do |i|
+      letter = self[i]
+      p_letter = self[i - 1]
+      if letter == "!" || letter == "." || letter == "?" && p_letter != "!" || p_letter != "." || p_letter != "?"
+        count += 1
+      end
+    end
   end
+  count
 end
